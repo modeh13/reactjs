07a1668183517151621a2c31ae7074ab4cdd1762
German Alexander Ramirez Vela <german.ca13@gmail.com>
2020-05-06 22:42:17 -0500

    Skeleton created.

---
+++ Gifos/src/components/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./app.less";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1> Hello, mi pimpolina! </h1>
+            </div>
+        );
+    }
+}
+
+export default App;
